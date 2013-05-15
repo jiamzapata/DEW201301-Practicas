49200dcaf7e592e17e0cd387cd4471339695d8a8
UPC_SEDE_SAN_BORJA <spantigo@upc.edu.pe>
2013-05-14 21:30:37 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = 'b9cfc4539810978a3d97cdfb314ae52b37a015420de843715c21745fd32de7bdfd045f4b94620ab7d9805abbac28693ce8d2cecb80621edef7b9f8fd373e8039'
